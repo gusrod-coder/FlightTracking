518931d658a088865d67e5ee4163007701c00a86
Gustavo Rodriguez <ger8170@gmail.com>
2026-08-28 16:02:32 -0400

    modified:   main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from FlightRadarAPI import FlightRadar24API
 
 fr_api = FlightRadar24API()
 
-bounds = fr_api.get_bounds_by_point(42.28574184424516, -83.71751929972811, 40000)
+bounds = fr_api.get_bounds_by_point(42.28574184424516, -83.71751929972811, 10000)
 flights = fr_api.get_flights(bounds = bounds)
 matrix = MatrixSimulator(128,64,15)
 
