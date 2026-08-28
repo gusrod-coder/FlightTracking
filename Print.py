bb3b1069146dd744c26f7998adf541de12cc639e
Gustavo Rodriguez <ger8170@gmail.com>
2026-08-27 22:23:31 -0400

    More details

--- Print.py
+++ Print.py
@@ -1,6 +1,16 @@
 from FlightRadarAPI import FlightRadar24API
 fr_api = FlightRadar24API()
 
-bounds = fr_api.get_bounds_by_point(42.28574184424516, -83.71751929972811, 10000)
+bounds = fr_api.get_bounds_by_point(42.28574184424516, -83.71751929972811, 15000)
 flights = fr_api.get_flights(bounds = bounds)
+
+for flight in flights:
+    flight_details = fr_api.get_flight_details(flight)
+    flight.set_flight_details(flight_details)
+    print("Flying to", flight.destination_airport_name)
+    print("Aircraft Age:", flight.aircraft_age)
+    print("Airline:", flight.airline_name)
+    print("Plane:", flight.aircraft_code)
+
+
 print(flights)
